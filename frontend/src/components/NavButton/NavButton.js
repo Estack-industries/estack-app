66dce6de70e6377c887564698c836ecf335b479c
Hyun <hyun.winter@gmail.com>
2021-09-25 13:34:49 -0700

    About Temporary Wave Background

--- frontend/src/components/NavButton/NavButton.js
+++ frontend/src/components/NavButton/NavButton.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 const MENUSTYLES = ['btn--primary', 'btn--dropdown'];
 const SIZES = ['btn--large', 'btn--medium', 'btn--small'];
+const DEFAULT_URL = ['#'];
 
 export const NavButton = ({
   children,
@@ -17,10 +18,13 @@ export const NavButton = ({
   const checkButtonSize = SIZES.includes(buttonSize)
     ? buttonSize
     : SIZES[0];
+  const checkURL = to
+    ? to
+    : DEFAULT_URL[0];
 
   return (
     <li>
-      <Link to={to} className={`btn ${checkButtonStyle} ${checkButtonSize}`}>
+      <Link to={`${checkURL}`} className={`btn ${checkButtonStyle} ${checkButtonSize}`}>
         {children}
       </Link>
     </li>
