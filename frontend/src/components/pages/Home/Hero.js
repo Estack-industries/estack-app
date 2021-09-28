7a1badca0242e3d5042cf8adf0b1a1cc4bfec25c
unknown <ni95@purdue.edu>
2021-09-28 15:47:23 -0400

    Home page styling

--- frontend/src/components/pages/Home/Hero.js
+++ frontend/src/components/pages/Home/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Hero.css'
 import landingHome from './pictures/house.png'
 import { FiSearch } from "react-icons/fi";
-
+import Ellipse from './pictures/ellipse.png'
 function Hero() {
   return (
     <div className="sectionWrapper">
@@ -16,6 +16,7 @@ function Hero() {
         </div>
         <img id="homeImage" src={landingHome} alt="Home page home" />
       </div>
+      <img className="ellipse1" src={Ellipse} alt="Ellipse"/>
     </div>
   )
 }
--- frontend/src/components/pages/Home/thirdSection.js
+++ frontend/src/components/pages/Home/thirdSection.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './thirdSection.css'
+import SectionLine from './pictures/sectionLine.png'
 import Agent from './pictures/agent.png'
 import Details from './pictures/details.png'
 import Deal from './pictures/deal.png'
@@ -19,101 +21,105 @@ function thirdSection() {
     return (
         <div className="sectionContainer">
             <div>
-                <p className="mainText">Why choose us?</p>
+                <div className="textDividerContainer">
+                    <img className="sectionLine" src={SectionLine} alt="Section Line"></img>
+                    <p className="mainText">Why choose us?</p>
+                    <img className="sectionLine" src={SectionLine} alt="Section Line"></img>
+                </div>
                 <div className="firstSectionContainer">
                     <div className="chooseBox">
                         <p className="boxImg"><img src={Agent} alt="Agent Pic" /></p>
-                        <p className="boxText">
-                            You’ll be in direct contact with the landlord/agent and he’ll help you sort out any doubts that you may have
-                        </p>
+                        <p className="boxText">You’ll be in direct contact with the landlord/agent and he’ll help you sort out any doubts that you may have</p>
                     </div>
                     <div className="chooseBox">
                         <p className="boxImg"><img src={Details} alt="Details Pic" /></p>
-                        <p className="boxText">
-                        We give you the details and every requirement that is needed for you to buy, rent or sell
-                        </p>
+                        <p className="boxText">We give you the details and every requirement that is needed for you to buy, rent or sell</p>
                     </div>
                     <div className="chooseBox">
                         <p className="boxImg"><img src={Deal} alt="Deal Pic" /></p>
-                        <p className="boxText">
-                            We help you close the deal of your dream home
-                        </p>
+                        <p className="boxText">We help you close the deal of your dream home</p>
                     </div>
                 </div>
             </div>
             <div className="section">
-                <p className="mainText">Top Listings</p>
+                <div className="textDividerContainer">
+                    <img className="sectionLine" src={SectionLine} alt="Section Line"></img>
+                    <p className="mainText">Top Listings</p>
+                    <img className="sectionLine" src={SectionLine} alt="Section Line"></img>
+                </div>
                 <div className="secondSectionContainer">
-                    <div className="listingBox">
-                        <div className="listingTop">
-                            <p className="listingImg"><img src={Listing1} alt="First Listing" /></p>
-                        </div>
-                        <div className="listingBottom">
-                            <p className="listingTitle">
-                                Ventura St. 3892, St Louis
-                            </p>
-                            <div className="listingStats">
-                                <p className="listingPrice">600.000$</p>
-                                <p className="listingDivider"><img src={ListingDivider} alt="divider"></img></p>
-                                <p className="listingStatus">New</p>
-                            </div>
-                            <div className="listingInfo">
-                                <ul className="listingInfoText">
-                                    <p className="listingIcon"><IoIosBed /></p> 4
-                                </ul>
-                                <ul className="listingInfoText">
-                                    <p className="listingIcon"><FaBath /></p> 2
-                                </ul>
-                                <ul className="listingInfoText">
-                                    <p className="listingIcon"><FiGrid /></p> 2
-                                </ul>
+                    <Link to="/" className="listingBoxContainer">
+                        <div className="listingBox">
+                            <img src={Listing1} alt="First Listing" className="cardImg" />
+                            <div className="listingBottom">
+                                <p className="listingTitle">Ventura St. 3892, St Louis</p>
+                                <div className="listingStats">
+                                    <p className="listingPrice">600.000$</p>
+                                    <p className="listingDivider"><img src={ListingDivider} alt="divider"></img></p>
+                                    <p className="listingStatus">New</p>
+                                </div>
+                                <div className="listingInfo">
+                                    <ul className="listingInfoText">
+                                        <p className="listingIcon"><IoIosBed /></p> 4
+                                    </ul>
+                                    <ul className="listingInfoText">
+                                        <p className="listingIcon"><FaBath /></p> 2
+                                    </ul>
+                                    <ul className="listingInfoText">
+                                        <p className="listingIcon"><FiGrid /></p> 2
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="listingBox">
-                        <p className="listingImg"><img src={Listing2} alt="Second Listing" /></p>
-                        <p className="listingTitle">
-                            Ventura St. 3892, St Louis
-                        </p>
-                        <div className="listingStats">
-                            <p className="listingPrice">600.000$</p>
-                            <p className="listingDivider"><img src={ListingDivider} alt="divider"></img></p>
-                            <p className="listingStatus">New</p>
+                    </Link>
+                    <Link to="/" className="listingBoxContainer">
+                        <div className="listingBox">
+                            <img src={Listing2} alt="Second Listing" className="cardImg" />
+                            <div className="listingBottom">
+                                <p className="listingTitle">Ventura St. 3892, St Louis</p>
+                                <div className="listingStats">
+                                    <p className="listingPrice">600.000$</p>
+                                    <p className="listingDivider"><img src={ListingDivider} alt="divider"></img></p>
+                                    <p className="listingStatus">New</p>
+                                </div>
+                                <div className="listingInfo">
+                                    <ul className="listingInfoText">
+                                        <p className="listingIcon"><IoIosBed /></p> 4
+                                    </ul>
+                                    <ul className="listingInfoText">
+                                        <p className="listingIcon"><FaBath /></p> 2
+                                    </ul>
+                                    <ul className="listingInfoText">
+                                        <p className="listingIcon"><FiGrid /></p> 2
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                        <div className="listingInfo">
-                            <ul className="listingInfoText">
-                                <p className="listingIcon"><IoIosBed /></p> 4
-                            </ul>
-                            <ul className="listingInfoText">
-                                <p className="listingIcon"><FaBath /></p> 2
-                            </ul>
-                            <ul className="listingInfoText">
-                                <p className="listingIcon"><FiGrid /></p> 2
-                            </ul>
+                    </Link>
+                    <Link to="/" className="listingBoxContainer">
+                        <div className="listingBox">
+                            <img src={Listing3} alt="Third Listing" className="cardImg"/>
+                            <div className="listingBottom">
+                                <p className="listingTitle">Ventura St. 3892, St Louis</p>
+                                <div className="listingStats">
+                                    <p className="listingPrice">600.000$</p>
+                                    <p className="listingDivider"><img src={ListingDivider} alt="divider"></img></p>
+                                    <p className="listingStatus">New</p>
+                                </div>
+                                <div className="listingInfo">
+                                    <ul className="listingInfoText">
+                                        <p className="listingIcon"><IoIosBed /></p> 4
+                                    </ul>
+                                    <ul className="listingInfoText">
+                                        <p className="listingIcon"><FaBath /></p> 2
+                                    </ul>
+                                    <ul className="listingInfoText">
+                                        <p className="listingIcon"><FiGrid /></p> 2
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="listingBox">
-                        <p className="listingImg"><img src={Listing3} alt="Third Listing" /></p>
-                        <p className="listingTitle">
-                            Ventura St. 3892, St Louis
-                        </p>
-                        <div className="listingStats">
-                            <p className="listingPrice">600.000$</p>
-                            <p className="listingDivider"><img src={ListingDivider} alt="divider"></img></p>
-                            <p className="listingStatus">New</p>
-                        </div>
-                        <div className="listingInfo">
-                            <ul className="listingInfoText">
-                                <p className="listingIcon"><IoIosBed /></p> 4
-                            </ul>
-                            <ul className="listingInfoText">
-                                <p className="listingIcon"><FaBath /></p> 2
-                            </ul>
-                            <ul className="listingInfoText">
-                                <p className="listingIcon"><FiGrid /></p> 2
-                            </ul>
-                        </div>
-                    </div>
+                    </Link>
                 </div>
             </div>
             <div className="section">
