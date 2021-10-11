55aed58211cdafd328620d7ae41c3a8d23b644cc
Hyun <hyun.winter@gmail.com>
2021-10-11 02:02:00 -0700

    Modal Wave Background Part 2

--- frontend/src/components/AuthWaveBackground/AuthWaveBackground.js
+++ frontend/src/components/AuthWaveBackground/AuthWaveBackground.js
@@ -7,19 +7,21 @@ import AuthWaveVector3 from '../../assets/AuthWaveVector 3.svg';
 export const AuthWaveBackground = () => {
   return (
     <div className="authwave-wrapper">
-      {/* <img className="authwave-wrapper-img" src={AuthWaveVector1} alt="AuthWaveVector1" />
+      {/* <img className="authwave-wrapper-img" src={AuthWaveVector3} alt="AuthWaveVector3" />
       <img className="authwave-wrapper-img" src={AuthWaveVector2} alt="AuthWaveVector2" />
-      <img className="authwave-wrapper-img" src={AuthWaveVector3} alt="AuthWaveVector3" /> */}
-      {/* <svg width="1442" height="583" viewBox="0 0 1199 500" fill="none" preserveAspectRatio="none">
-        <path fill="#CBE9FF" d="M1179.34 715.274C1330.21 733.312 1409.08 574.19 1434.66 518L1438.66 -19L-0.335938 12.0001L-0.33592 200.971C-0.335913 286.867 60.456 570.773 265.664 523C454.664 479 549.664 715.274 768.664 607C996.092 494.559 990.751 692.726 1179.34 715.274Z" />
-        <path fill="url(#paint0_linear)" d="M1274.67 578C1422.13 611.777 1437.17 458.347 1462.66 406L1440.67 -10L0.664062 -9.99987L0.664103 389C0.664103 469.021 201.024 547.224 393.028 462.574C591.665 375 621.243 616.031 839.668 497C1058.09 377.969 1095.67 537 1274.67 578Z" />
-        <path fill="#98D4FF" d="M1044.66 438.5C1160.16 526 1302.66 583.5 1387.16 539C1387.16 539 1418.16 521 1437.16 495C1456.16 469 1480.66 438.5 1480.66 438.5L1525.66 350L1483.66 74.9999L-35.3359 0C-35.3359 33.7009 35.7363 233.815 15.1643 298C-9.83574 376 30.287 491.316 238.164 462C472.164 429 466.664 367 636.664 470C833.094 589.013 932.466 353.502 1044.66 438.5Z" />
+      <img className="authwave-wrapper-img" src={AuthWaveVector1} alt="AuthWaveVector1" /> */}
 
-        <linearGradient id="paint0_linear" x1="0.663636" y1="-10" x2="1440.66" y2="511.5" gradientUnits="userSpaceOnUse">
-          <stop stop-color="#0193FD" />
-          <stop offset="1" stop-color="#3F51F4" />
-        </linearGradient>
-      </svg> */}
+      <div className="authwave-wrapper-img">
+        <svg viewBox="0 0 594 1442" fill="none" preserveAspectRatio="none">
+          <path d="M725.836 271.101C743.874 120.231 535.063 9.93701 473.563 0.937012H0.5625L0.0625 1441.94H213.562C299.459 1441.94 581.335 1389.98 533.562 1184.78C489.562 995.777 725.837 900.777 617.562 681.777C505.121 454.349 703.288 459.69 725.836 271.101Z" fill="#CBE9FF" />
+          <path d="M521.293 442.971C608.666 331.67 653.535 204.465 618.535 114.965C618.535 114.965 599.035 68.9648 557.535 39.9648C516.035 10.9648 468.535 0.464844 468.535 0.464844H443.035H401.535H0.535156L0.535156 1441.46C34.187 1441.46 313.573 1441.46 382.035 1441.46C462.035 1441.46 574.032 1420.47 544.759 1220.15C511.807 994.653 449.897 999.953 552.747 836.134C671.587 646.845 436.419 551.086 521.293 442.971Z" fill="#98D4FF" />
+          <path d="M588.801 166.993C618.309 38.1626 485.801 0.162598 426.301 0.162598H207.301H0.300781L0.300781 1441.16L399.801 1441C479.822 1441 558.025 1240.64 473.375 1048.64C385.801 849.998 626.832 820.42 507.801 601.995C388.77 383.573 547.801 345.993 588.801 166.993Z" fill="url(#paint0_linear)" />
+          <linearGradient id="paint0_linear" x1="0.663636" y1="-10" x2="1440.66" y2="511.5" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#0193FD" />
+            <stop offset="1" stop-color="#3F51F4" />
+          </linearGradient>
+        </svg>
+      </div>
     </div>
   )
 };
