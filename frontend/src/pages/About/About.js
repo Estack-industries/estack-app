2d93b152cfb5c2d0cb3dc577730c33223f99cdbd
benily <leekwokhinben@gmail.com>
2021-12-09 23:38:25 +0800

    about update

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -8,6 +8,8 @@ import AboutMainImage from './assets/about-main-image.png';
 import AboutVector1 from './assets/about-vector-1.svg';
 import AboutVector2 from './assets/about-vector-2.svg';
 import AboutVector3 from './assets/about-vector-3.svg';
+import Click from './assets/click.svg';
+import AboutPhoto1 from './assets/about-photo-1.png';
 
 function NavBar() {
 	return (
@@ -19,16 +21,16 @@ function NavBar() {
 				onClick={() => console.log('clicked')}
 			/>
 			<div className="navbar-options">
-				<a className="navbar-option" href="#">
+				<a className="navbar-option white-color open-sans" href="#">
 					Buy
 				</a>
-				<a className="navbar-option" href="#">
+				<a className="navbar-option white-color open-sans" href="#">
 					Sell
 				</a>
-				<a className="navbar-option" href="#">
+				<a className="navbar-option white-color open-sans" href="#">
 					Rent
 				</a>
-				<a className="navbar-option" href="#">
+				<a className="navbar-option white-color open-sans" href="#">
 					About Us
 				</a>
 			</div>
@@ -62,6 +64,69 @@ const About = () => {
 		<div>
 			<Background />
 			<NavBar />
+
+			<div className="full-width">
+				<p className="white-color large-text dm-sans medium-padding-left">
+					About Us
+				</p>
+			</div>
+
+			<div className="full-width flex flex-end">
+				<div className="lato small-text half-width align-right flex column-flex blue-border-box medium-margin-horizontal">
+					<p className="medium-padding-horizontal medium-padding-vertical center-text">
+						E-Stack
+					</p>
+					<p className="medium-padding-horizontal small-padding-vectical center-text">
+						We offer this complimetary service because we’re
+						confident you are going to love E-Stack.
+					</p>
+					<p className="medium-padding-horizontal small-padding-vectical center-text">
+						There are no hidden fees and there’s never any
+						obligation to move foward
+					</p>
+				</div>
+			</div>
+
+			<div className="small-space" />
+
+			<div className="full-width flex small-padding-vectical">
+				<div className="half-width">
+					<p className="small-text center-text dm-sans">
+						Get a Free Estimate
+					</p>
+				</div>
+			</div>
+
+			<div className="full-width flex">
+				<div className="half-width flex column-flex">
+					<div className="full-width flex flex-center">
+						<input
+							className="email-input center-text"
+							placeholder="Enter your Email Address"
+							required
+						/>
+					</div>
+					<img
+						className="email-input-sumbit-button"
+						src={Click}
+						alt="click-button"
+						onClick={() => console.log('submit')}
+					/>
+				</div>
+
+				<div className="half-width">
+					<img
+						className="full-width medium-padding-horizontal about-photo-1"
+						src={AboutPhoto1}
+						alt="about-photo-1"
+					/>
+				</div>
+			</div>
+			<div className="small-space" />
+			<div className="small-space" />
+			<div className="small-space" />
+			<div className="small-space" />
+			<div className="small-space" />
 		</div>
 	);
 };
