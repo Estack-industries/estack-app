68a9c649fd5f81dbd8a3929d06f00d9f6b4d9939
KyleSmith0905 <kylesmith090502@gmail.com>
2021-12-20 23:17:20 -0500

    Fix: More compliant rules regarding cards. Online, it says cvv's could be 3-4 numbers and card-number could be 8-19 numbers.

--- backend/mysql/estackdb.sql
+++ backend/mysql/estackdb.sql
@@ -30,8 +30,8 @@ unique key(uname)
 
 create table owners (
 email VARCHAR(40) NOT NULL,
-cvv VARCHAR(3),
-card_number VARCHAR(20),
+cvv VARCHAR(4) CHECK(length(cvv) = 3 or length(cvv) = 4),
+card_number VARCHAR(23) CHECK(length(card_number) >= 9),
 expiration_date DATE,
 upgraded BOOLEAN default false,
 PRIMARY KEY (email)
@@ -49,15 +49,15 @@ PRIMARY KEY(email)
 
 create table customers (
 email VARCHAR(40) NOT NULL,
-cvv VARCHAR(3),
-card_number VARCHAR(20),
+cvv VARCHAR(4) CHECK(length(cvv) = 3 or length(cvv) = 4),
+card_number VARCHAR(23) CHECK(length(card_number) >= 9),
 expiration_date DATE,
 PRIMARY KEY (email)
 ) engine = innodb;
 
 
 create table properties (
-    -- address
+	-- address
     street VARCHAR(200) NOT NULL,
     city VARCHAR(85) NOT NULL,
     state CHAR(2) NOT NULL CHECK(length(State) = 2),
@@ -92,11 +92,13 @@ create table properties (
 
 -- users (uname, email, password, fname, lname, birthdate, street, city, state, zip_code)
 insert into users values
-('jstone5', 'jared@gmail.com', 'passwordP!', 'Jared', 'Stone', '1961-01-06', '101 Five Finger Way', 'Atlanta', 'Georgia', '30333'),
+('jstone5', 'jared@gmail.com', 'passwordP!', 'Jared', 'Stone', '1961-01-06', '101 Five Finger Way', 'Atlanta', 'GA', '30333'),
+('johnDoe1', 'JohnD1@gmail.com', 'passwordP!', 'John', 'Doe', '1999-12-31', '123 Maple Street', 'Anytown', 'PA', '20522'),
 (NULL, 'sarahprince@gmail.com', 'passwordP!', 'Sarah', 'Prince', NULL,  NULL, NULL, NULL, NULL);
 
 insert into owners values
-('jared@gmail.com', '123', '4040 4040 4040 4040', '1920-03-01', false);
+('jared@gmail.com', '123', '4040 4040 4040 4040 404', '1920-03-01', false),
+('JohnD1@gmail.com', '1234', '1234 5678', '2022-01-01', false);
 
 insert into properties values
 ('Front Street', 'Ann Arbor', 'MI', '40044', "Apartment", 2, 2, 860, '1980-01-01', 5, true, true, true, true, true, true, 200000.43, "White House", "jared@gmail.com", "This is the first selling house!", NULL);
