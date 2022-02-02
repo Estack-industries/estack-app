bbf1f97c7ed27501d7fd90638082cf876fb86592
KyleSmith0905 <kylesmith090502@gmail.com>
2022-02-02 11:48:49 -0500

    Made frontend Property Summary component. They are boxes with words standard info like baths beds and sqrft

--- frontend/src/pages/Account/components/favorited.js
+++ frontend/src/pages/Account/components/favorited.js
@@ -1,18 +1,5 @@
 import styles from './favorited.module.css';
-import {moneyFormat} from '../utils';
-
-import houseBeds from '../assets/house-beds.svg';
-import houseBaths from '../assets/house-baths.svg'
-import houseSqfts from '../assets/house-sqfts.svg';
-
-const HomeStatistics = function({text, icon, alt}) {
-	return (
-		<p>
-			<img src={icon} alt={alt}/>
-			{text}
-		</p>
-	)
-}
+import PropertySummary from '../../../components/PropertySummary'
 
 function FavoritedHomes({homes}) {
 
@@ -20,17 +7,7 @@ function FavoritedHomes({homes}) {
 		<div id={styles.favoritedHomes}>
 			<div>
 				{homes.map((home, index) => 
-					<a className={styles.favoritedHome} key={index} href={'/buy/' + home.id}>
-						<img src={home.image} alt={'home at ' + home.address}/>
-						<p className={styles.homeAddress}>{home.address}</p>
-						<p className={styles.homePrice}>{moneyFormat(home.price)}</p>
-						{home.new && <p className={styles.newTag}>New</p>}
-						<div className={styles.stats}>
-							<HomeStatistics text={home.beds} icon={houseBeds} alt='Number of beds'/>
-							<HomeStatistics text={home.baths} icon={houseBaths} alt='Number of baths'/>
-							<HomeStatistics text={home.sqft} icon={houseSqfts} alt='Square footage'/>
-						</div>
-					</a>
+					<PropertySummary homeData={home} key={index}/>
 				)}
 			</div>
 		</div>
