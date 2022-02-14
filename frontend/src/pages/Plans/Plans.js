2ef0843d13f4e2da416e6d82d9086648e05bd63f
KyleSmith0905 <kylesmith090502@gmail.com>
2022-02-14 17:14:56 -0500

    Updated styling on `/plans` page for site consistency

--- frontend/src/pages/Plans/Plans.js
+++ frontend/src/pages/Plans/Plans.js
@@ -1,111 +1,106 @@
 import React from 'react'; 
 import Navbar from '../../components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
-import './Plans.css';
+import styles from './Plans.module.css';
 import Background from '../../components/NavBackground';
+import Footer from '../../components/Footer/Footer';
 
 const Plans = () => {
 	return (
 		<div>
 			<Background />
 			<Navbar />
-			<h1 className="title">E-Stack Plans</h1>
-			<div className="large-space" />
-			<div className="large-space" />
-			<div className="large-space" />
-			<div className="large-space" />
-			<div className="large-space" />
-			<div className="large-space" />
-			<div className="large-space" />
-			<div class="row">
-				<div class="col-sm-6">
-					<div className="margin" />
-					<div class="card">
-						<div class="card-body">
-							<h4 class="card-title">Basic Plan</h4>
+			<h1 className={styles.header}>E-Stack Plans</h1>
+			<div class="row" style={{margin: '0em 3.5em'}}>
+				<div class={`col-sm-6 ${styles.cardContainer}`}>
+					<div class={`card ${styles.card}`}>
+						<div class={styles.cardBody}>
+							<h4 class={styles.cardTitle}>Basic Plan</h4>
 							<div className="small-space" />
-							<h5 class="card-subtitle">Free</h5>
+							<h5 class={styles.cardPrice}>Free</h5>
 							<hr />
-							<span class="check">&#10003;</span>
-							<span>&emsp;You can message the agents</span>
+							<span class={styles.check}>&#10003;</span>
+							<p>&emsp;You can message the agents</p>
 							<div className="small-space" />
-							<span class="check">&#10003;</span>
-							<span>
+							<span class={styles.check}>&#10003;</span>
+							<p>
 								&emsp;You can navigate and register for free
-							</span>
+							</p>
 							<div className="small-space" />
-							<span class="check">&#10003;</span>
-							<span>
+							<span class={styles.check}>&#10003;</span>
+							<p>
 								&emsp;You can search for your new lease for free
-							</span>
+							</p>
 							<div className="small-space" />
-							<span class="check">&#10003;</span>
-							<span>
+							<span class={styles.check}>&#10003;</span>
+							<p>
 								&emsp;You can only sell 1 property at a time
-							</span>
+							</p>
 							<div className="small-space" />
-							<span class="check">&#10003;</span>
-							<span>&emsp;Help from our team 24/7</span>
+							<span class={styles.check}>&#10003;</span>
+							<p>&emsp;Help from our team 24/7</p>
 							<div className="small-space" />
 							<span class="space">&emsp;&emsp;</span>
 							<a href="#">Learn More</a>
 						</div>
 					</div>
 				</div>
-				<div class="col-sm-6">
-					<div class="label">Recommended Plan</div>
-					<div class="card">
-						<div class="card-body">
-							<h4 class="card-title">Premium Plan</h4>
+				<div class={`col-sm-6 ${styles.cardContainer}`}>
+					<div class={styles.label}>Recommended Plan</div>
+					<div class={`card ${styles.card}`}>
+						<div class={styles.cardBody}>
+							<h4 class={styles.cardTitle}>Premium Plan</h4>
 							<div className="small-space" />
-							<h5 class="card-subtitle">20$</h5>
-							<h6 class="card-detail">
+							<h5 class={styles.cardPrice}>20$</h5>
+							<h6 class={styles.cardDetail}>
 								Per month, paid annually
 							</h6>
 							<hr />
-							<span class="check">&#10003;</span>
-							<span>
+							<span class={styles.check}>&#10003;</span>
+							<p>
 								&emsp;You can be in direct contact with the
 								seller and call if you want to
-							</span>
+							</p>
 							<div className="small-space" />
-							<span class="check">&#10003;</span>
-							<span>
+							<span class={styles.check}>&#10003;</span>
+							<p>
 								&emsp;Exclusive discounts and promotions inside
 								the platform
-							</span>
+							</p>
 							<div className="small-space" />
-							<span class="check">&#10003;</span>
-							<span>
+							<span class={styles.check}>&#10003;</span>
+							<p>
 								&emsp;We help you with the marketing of your
 								listings
-							</span>
+							</p>
 							<div className="small-space" />
-							<span class="check">&#10003;</span>
-							<span>
+							<span class={styles.check}>&#10003;</span>
+							<p>
 								&emsp;No limits in the amount of properties that
 								you can list on our platform
-							</span>
+							</p>
 							<div className="small-space" />
-							<span class="check">&#10003;</span>
-							<span>
+							<span class={styles.check}>&#10003;</span>
+							<p>
 								&emsp;You'll receive our recommendations to
 								sell/buy/lease efficiently
-							</span>
+							</p>
 							<div className="small-space" />
-							<span class="check">&#10003;</span>
-							<span>&emsp;Help from our team 24/7</span>
+							<span class={styles.check}>&#10003;</span>
+							<p>&emsp;Help from our team 24/7</p>
 							<div className="small-space" />
 							<span class="space">&emsp;&emsp;</span>
 							<a href="#">Learn More</a>
 							<div className="small-space" />
-							<button type="button" class="btn btn-warning">
+							<button type="button" class={`${styles.btn} btn-warning`}>
 								Try our premium plan
 							</button>
 						</div>
 					</div>
 				</div>
 			</div>
+			<div className="large-space" />
+			<Footer />
 		</div>
 	);
 };
